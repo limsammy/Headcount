844702372bc53d3355df60767a4e7b5bc55c8ff1
Chris Smith <42@chrisryansmith.com>
2017-04-11 11:21:32 -0600

    working with CSV object to query

--- lib/csv_query.rb
+++ lib/csv_query.rb
@@ -6,35 +6,28 @@ module CSVQuery
     CSV.open file_name, headers:true, header_converters: :symbol
   end
 
-  def parse_file(file_name, header)
+  def parse_file(file_name, query)
     contents = load_file(file_name)
-    sanitize(contents, header)
+    sanitize(contents)
+    find_records(contents, query)
   end
 
-  def get_column(file_name, header)
-    contents = load_file(file_name)
-
-    contents.map do |row|
-      row[header]
+  def find_records(contents,query)
+    search_in = query[:search_in].to_sym
+    search_for = query[:find]
+    result = contents.select do |row|
+      row[search_in] == search_for
     end
+    csv_to_hash(result)
   end
 
-  def csv_to_hash(file_name)
-    contents = load_file(file_name)
-
-    contents.to_a.map {|row| row.to_hash } #returns array of hashes, each hash storing data relevant to the district
-  end
-
-  def sanitize(contents, header)
-    if header == :data && get_column(contents, header)[0].include?(".")
-      csv_to_hash(contents).each do |hash|
-        hash[:data] = format_percent(hash[:data])
-        hash
+  def sanitize(contents)
+    contents.each do |row|
+      if row[:dataformat] == "Percent"
+        row[:data] = format_percent(row[:data])
       end
-    elsif header == :location
-      csv_to_hash(contents).each do |hash|
-        hash[:location] = hash[:location].upcase
-        hash
+      if row[:location] != "Colorado"
+        row[:location] = row[:location].upcase
       end
     end
   end
@@ -42,4 +35,14 @@ module CSVQuery
   def format_percent(number)
     number.to_f.round(3)
   end
+
+  def csv_to_hash(contents)
+    contents.to_a.map {|row| row.to_hash } #returns array of hashes, each hash storing data relevant to the district
+  end
 end
+
+# def get_column(contents, header)
+#   contents.map do |row|
+#     row[header]
+#   end
+# end
--- lib/district_repository.rb
+++ lib/district_repository.rb
@@ -1,4 +1,6 @@
 require_relative 'district'
+require_relative 'csv_query'
+require_relative 'enrollment_repo'
 
 class DistrictRepository
   attr_reader :enrollment_repo, :testing_repo, :econ_repo, :data
@@ -11,15 +13,26 @@ class DistrictRepository
   end
 
   def load_data(args)
+    if args.key?[:enrollment]
+      @enrollment_repo = EnrollmentRepository.new if @enrollment_repo.nil?
+      district_data = @enrollment_repo.load_data({enrollment: args[:enrollment]})
+      populate_data(district_data)
+    end
     # populate @data keys with district names
     # create and populate _repo if needed
     # send appropriate data to repo
   end
 
+  def populate_data(districts)
+    districts.uniq.each do |district|
+      find_by_name(district)
+    end
+  end
+
   def find_by_name(name)
-    name = name.upcase
-    create_district(name) unless data.key?(name)
-    data[name]
+    name = name.upcase unless name == "Colorado"
+    create_district(name) if data[name].nil?
+    data[name] ||= create_district(name)
   end
 
   def find_all_matching(text)
@@ -30,6 +43,6 @@ class DistrictRepository
   end
 
   def create_district(name)
-    data[name] = District.new({name: name, repo: self})
+    District.new({name: name, repo: self})
   end
 end
