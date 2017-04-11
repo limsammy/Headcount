2beb8e41fa9d42dafde254a0a39f254779fe7c38
Chris Smith <42@chrisryansmith.com>
2017-04-11 12:27:48 -0600

    update enrollment_repository

--- lib/enrollment_repository.rb
+++ lib/enrollment_repository.rb
@@ -1,28 +1,58 @@
 require_relative 'enrollment'
+require_relative 'csv_query'
 
 class EnrollmentRepository
+  include CSVQuery
   attr_reader :data
 
   def initialize
-    @data = {}
+    @data = []
   end
 
   def load_data(args)
-    args[:enrollment].each do |file|
-
+    args[:enrollment].each do |category, file|
+      contents = parse_file(file)
+      process_data(contents, category)
     end
     # populate @data keys with district names
     # parse appropriate data and create/store enrollment objects
   end
 
-  def find_by_name(name)
-    name = name.upcase
-    create_enrollment(name) unless data.key?(name)
-    data[name]
+  def translate_category(category)
+    categories = {
+      kindergarten: :kindergarten_participation,
+    }
+    categories[category]
   end
 
-  def create_enrollment(name, enrollment_data)
-    data[name] = Enrollment.new({name: name, enrollment_data})
-    # set district object on Enrollment?
+  def process_data(contents, category)
+    data_category = translate_category(category)
+    contents.each do |row|
+      if row[:dataformat] == "Percent"
+        row[:data] = format_percent(row[:data])
+      end
+      enrollment_data = {
+        :name => row[:location],
+        data_category => {row[:timeframe] => row[:data]}
+      }
+      if find_by_name(row[:location])
+        # append current data to enrollment object
+        enrollment = find_by_name(row[:location])
+        enrollment.update_data(enrollment_data)
+      else
+        create_enrollment(enrollment_data)
+      end
+      # create enrollment object for each location
+      # - check @data if EO already exists.  If it does append new data to category
+      # - if not there, create it
+    end
+  end
+
+  def find_by_name(name)
+    data.find { |enrollment| enrollment.name == name }
+  end
+
+  def create_enrollment(enrollment_data)
+    data << Enrollment.new(enrollment_data)
   end
 end
