9b9893caa7e08e46e1e290113f1da87f9dd709b5
Sam Lim <limsammy1@gmail.com>
2017-04-11 14:26:28 -0600

    all tests passing for all of module CSVParser

--- test/csv_parser_test.rb
+++ test/csv_parser_test.rb
@@ -1,59 +1,69 @@
 require_relative 'test_helper'
-require_relative '../lib/csv_query'
+require_relative '../lib/csv_parser'
 
 class DummyClass
-  include CSVQuery
+  include CSVParser
 end
 
 class CSVQueryTest < Minitest::Test
-  def test_load_file_method
-    skip
-    assert_respond_to(DummyClass.new, :load_file)
+  def setup
+    @dummy = DummyClass.new
   end
 
-  def test_load_file_returns_csv_object_of_loaded_file
-    skip
-    assert_instance_of CSV, DummyClass.new.load_file("./data/Special education.csv")
+  def test_respond_to_parse_file
+    assert_respond_to(@dummy, :parse_file)
   end
 
-  def test_get_column_returns_indiciated_column
+  def test_respond_to_format_percent
+    assert_respond_to(@dummy, :format_percent)
+  end
+
+  def test_respond_to_csv_to_hash # Method unused as of iteration 0
+    assert_respond_to(@dummy, :csv_to_hash)
+  end
+
+  def test_respond_find_records
+    assert_respond_to(@dummy, :find_records)
+  end
+
+  def test_parse_file_returns_csv_object
+    assert_instance_of CSV, @dummy.parse_file("./data/Special education.csv")
+  end
+
+  def test_get_column_returns_indiciated_column # unused as of iteration 0
     skip
-    contents = DummyClass.new.load_file("./data/Special education.csv")
-    column = DummyClass.new.get_column(contents, :location)
+    contents = @dummy.parse_file("./data/Special education.csv")
+    column = @dummy.get_column(contents, :location)
     column = column[0].to_s
 
     assert_includes "Colorado", column
   end
 
-  def test_csv_to_hash_returns_hash_of_data
+  def test_csv_to_hash_returns_array_of_hashes_of_data # unused as of iteration 0
     skip
-    contents = DummyClass.new.load_file("./data/Special education.csv")
-    assert_equal 1086, DummyClass.new.csv_to_hash(contents).count
+    contents = @dummy.parse_file("./data/Special education.csv")
+    data = @dummy.csv_to_hash(contents)
+    assert_instance_of Array, @dummy.csv_to_hash(contents)
+    assert_equal 1086, @dummy.csv_to_hash(contents).count
+    data.each do |district_hash|
+      assert_instance_of Hash, district_hash
+    end
   end
 
   def test_format_percent_creates_float_and_rounds_to_3_decimals
-    skip
     sample_num = 2.545195857435
-    assert_instance_of Float, DummyClass.new.format_percent(sample_num)
-    assert_equal 2.545, DummyClass.new.format_percent(sample_num)
+    assert_instance_of Float, @dummy.format_percent(sample_num)
+    assert_equal 2.545, @dummy.format_percent(sample_num)
   end
 
-  def test_sanitize_data_header
-    skip
-    contents = DummyClass.new.load_file("./data/Special education.csv")
-    assert_equal 0.101, DummyClass.new.sanitize(contents, :data) #[4] # [:data]
-  end
-
-  def test_sanitize_location_case
-    skip
-    contents = DummyClass.new.load_file("./data/Special education.csv")
-    assert_equal "COLORADO", DummyClass.new.sanitize(contents, :location)[0][:location]
-  end
-
-  def test_find_specific_records
-    contents = DummyClass.new.load_file("./data/Special education.csv")
+  def test_find_specific_records_returns_array_of_csv_objects
+    contents = @dummy.parse_file("./data/Special education.csv")
     search = {search_in: "location", find:"Colorado"}
-    expected = [{:location=>"Colorado", :timeframe=>"2009", :dataformat=>"Percent", :data=>"0.096"}, {:location=>"Colorado", :timeframe=>"2011", :dataformat=>"Percent", :data=>"0.097"}, {:location=>"Colorado", :timeframe=>"2012", :dataformat=>"Number", :data=>"84410"}, {:location=>"Colorado", :timeframe=>"2012", :dataformat=>"Percent", :data=>"0.098"}, {:location=>"Colorado", :timeframe=>"2013", :dataformat=>"Percent", :data=>"0.10056"}, {:location=>"Colorado",:timeframe=>"2010",:dataformat=>"Percent", :data=>"0.096"}, {:location=>"Colorado", :timeframe=>"2014", :dataformat=>"Number", :data=>"89602"}, {:location=>"Colorado", :timeframe=>"2014", :dataformat=>"Percent", :data=>"0.10079"}]
-    assert_equal expected, DummyClass.new.find_records(contents, search)
+    records = @dummy.find_records(contents, search)
+    assert_instance_of Array, records
+    assert_equal 8, records.count
+    records.each do |csv_obj|
+      assert_instance_of CSV::Row, csv_obj
+    end
   end
 end
