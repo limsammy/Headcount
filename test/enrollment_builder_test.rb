e889be42626e228c492a8faf44a382d6f9e987a1
Chris Smith <42@chrisryansmith.com>
2017-04-13 15:06:14 -0600

    fix enrollment builder test to include real EnrollmentRepository

--- test/enrollment_builder_test.rb
+++ test/enrollment_builder_test.rb
@@ -4,19 +4,21 @@ class EnrollmentBuilderTest < MiniTest::Test
   attr_reader :eb
 
   def setup
-    @eb = EnrollmentBuilder.new
+    @er = EnrollmentRepository.new
+    @eb = EnrollmentBuilder.new(@er)
+    @enrollment_data = {:name => "DISTRICT 1", :kindergarten_participation => {2011 => 0.35356}}
   end
 
   def test_responds_to_create_enrollment
-    assert_respond_to(@er, :create_enrollment)
+    assert_respond_to(@eb, :create_enrollment)
   end
 
   def test_responds_to_process_data
-    assert_respond_to(@er, :process_data)
+    assert_respond_to(@eb, :process_data)
   end
 
   def test_create_enrollment_adds_enrollment_object_to_data
-    enrollment = @er.create_enrollment(@enrollment_data)
+    enrollment = @eb.create_enrollment(@enrollment_data)
     assert_equal 1, @er.data.length
     assert_instance_of Enrollment, @er.data[0]
   end
