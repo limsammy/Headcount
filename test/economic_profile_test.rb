0a6c204dd1df1a18666c5e9b40b27a306039d35d
Chris Smith <42@chrisryansmith.com>
2017-04-13 20:30:04 -0600

    economic profile methods

--- test/economic_profile_test.rb
+++ test/economic_profile_test.rb
@@ -7,9 +7,9 @@ class EconomicProfileTest < MiniTest::Test
     @ep = EconomicProfile.new(
       {name: 'Test',
         :median_household_income => {[2005, 2009] => 50000, [2008, 2012] => 60000},
-        :children_in_poverty => {2012 => 0.1845},
-        :free_or_reduced_price_lunch => {2014 => {:percentage => 0.023, :total => 100}},
-        :title_i => {2015 => 0.543}
+        :children_in_poverty => {2000 => 0.1845, 2012 => 0.2045},
+        :free_or_reduced_price_lunch => {2014 => {:percentage => 0.023, :total => 100}, 2001 => {:percentage => 0.203, :total => 1000}},
+        :title_i => {2014 => 0.543}
         }
       )
   end
@@ -48,23 +48,96 @@ class EconomicProfileTest < MiniTest::Test
     assert_respond_to(@ep, :median_household_income_in_year)
   end
 
+  def test_median_income_in_year_return_error_for_unknown_year
+    assert_raises(UnknownDataError){@ep.median_household_income_in_year(2004)}
+    assert_raises(UnknownDataError){@ep.median_household_income_in_year(2014)}
+  end
+
+  def test_median_income_in_year_returns_integer
+    assert_instance_of Fixnum, @ep.median_household_income_in_year(2005)
+  end
+
+  def test_median_income_in_year_returns_correct_average
+    assert_equal 55000, @ep.median_household_income_in_year(2008)
+    assert_equal 60000, @ep.median_household_income_in_year(2010)
+  end
+
   def test_respond_to_median_household_income_average
     assert_respond_to(@ep, :median_household_income_average)
+  end
+
+  def test_median_household_income_averages_known_incomes
+    assert_equal 55000, @ep.median_household_income_average
   end
 
   def test_respond_to_children_in_poverty_in_year
     assert_respond_to(@ep, :children_in_poverty_in_year)
   end
 
+  def test_children_in_poverty_raises_error_for_unknown_year
+    assert_raises(UnknownDataError){@ep.children_in_poverty_in_year(1994)}
+    assert_raises(UnknownDataError){@ep.children_in_poverty_in_year(2014)}
+  end
+
+  def test_children_in_poverty_in_year_returns_float
+    assert_instance_of Float, @ep.children_in_poverty_in_year(2000)
+  end
+
+  def test_children_in_poverty_in_year_returns_value
+    assert_equal 0.1845, @ep.children_in_poverty_in_year(2000)
+    assert_equal 0.2045, @ep.children_in_poverty_in_year(2012)
+  end
+
   def test_respond_to_free_or_reduced_price_lunch_percentage_in_year
     assert_respond_to(@ep, :free_or_reduced_price_lunch_percentage_in_year)
+  end
+
+  def test_free_or_reduced_price_lunch_percentage_raises_error_for_unknown_year
+    assert_raises(UnknownDataError){@ep.free_or_reduced_price_lunch_percentage_in_year(1999)}
+    assert_raises(UnknownDataError){@ep.free_or_reduced_price_lunch_percentage_in_year(2015)}
+  end
+
+  def test_free_or_reduced_price_lunch_percentage_in_year_returns_integer
+    assert_instance_of Float, @ep.free_or_reduced_price_lunch_percentage_in_year(2001)
+  end
+
+  def test_free_or_reduced_price_lunch_percentage_in_year_returns_value
+    assert_equal 0.023, @ep.free_or_reduced_price_lunch_percentage_in_year(2014)
+    assert_equal 0.203, @ep.free_or_reduced_price_lunch_percentage_in_year(2001)
   end
 
   def test_respond_to_free_or_reduced_price_lunch_number_in_year
     assert_respond_to(@ep, :free_or_reduced_price_lunch_number_in_year)
   end
 
+  def test_free_or_reduced_price_lunch_number_raises_error_for_unknown_year
+    assert_raises(UnknownDataError){@ep.free_or_reduced_price_lunch_number_in_year(1999)}
+    assert_raises(UnknownDataError){@ep.free_or_reduced_price_lunch_number_in_year(2015)}
+  end
+
+  def test_free_or_reduced_price_lunch_number_in_year_returns_integer
+    assert_instance_of Fixnum, @ep.free_or_reduced_price_lunch_number_in_year(2001)
+  end
+
+  def test_free_or_reduced_price_lunch_number_in_year_returns_value
+    assert_equal 100, @ep.free_or_reduced_price_lunch_number_in_year(2014)
+    assert_equal 1000, @ep.free_or_reduced_price_lunch_number_in_year(2001)
+  end
+
   def test_respond_to_title_i_in_year
     assert_respond_to(@ep, :title_i_in_year)
+  end
+
+  def test_title_i_in_year_raises_error_for_unknown_year
+    assert_raises(UnknownDataError){@ep.title_i_in_year(1999)}
+    assert_raises(UnknownDataError){@ep.title_i_in_year(2015)}
+  end
+
+  def test_title_i_in_year_returns_integer
+    assert_instance_of Float, @ep.title_i_in_year(2014)
+  end
+
+  def test_title_i_in_year_returns_value
+    assert_equal 0.543, @ep.title_i_in_year(2014)
   end
 end
