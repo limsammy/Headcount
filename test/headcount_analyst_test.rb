7d11f6974ddb1d1aef826e56a7f1162949c8fb14
Sam Lim <limsammy1@gmail.com>
2017-04-17 11:29:20 -0600

    cleanup

--- test/headcount_analyst_test.rb
+++ test/headcount_analyst_test.rb
@@ -100,15 +100,14 @@ class HeadcountAnalystTest < MiniTest::Test
     assert_equal 'ACADEMY 20', @ha_test.get_districts_and_growths(3, :math)[1][:name]
   end
 
-  # def test_can_find_top_statewide_test_year_over_year_growth
-  #   skip
-  #   expected = 0.123
-  #   result = @ha_test.top_statewide_test_year_over_year_growth(grade: 3, subject: :math)
-  #   assert_equal expected, result
-  # end
-
   def test_can_find_single_top_district_growth
     expected = ['SPRINGFIELD RE-4', 0.14916666666666667]
     assert_equal expected, @ha_test.find_single_top_district_growth(@ha_test.get_districts_and_growths(3, :math))
+  end
+
+  def test_can_find_top_statewide_test_year_over_year_growth
+    expected = ['SPRINGFIELD RE-4', 0.14916666666666667]
+    result = @ha_test.top_statewide_test_year_over_year_growth(grade: 3, subject: :math)
+    assert_equal expected, result
   end
 end
